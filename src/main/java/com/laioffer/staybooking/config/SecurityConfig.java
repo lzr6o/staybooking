c402f783b9edc275ab88f8d26cf2736fa96ee107
李zr <zr6oli@gmail.com>
2022-08-10 18:43:16 -0400

    User Registration

--- src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
+++ src/main/java/com/laioffer/staybooking/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.laioffer.staybooking.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,5 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/register/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf()
+                .disable();
     }
 }
